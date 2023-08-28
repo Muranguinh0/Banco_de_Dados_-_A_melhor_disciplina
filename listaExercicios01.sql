1f01a4e757a4cb00779f398b4abd23f369032011
Muranguinh0 <116584163+Muranguinh0@users.noreply.github.com>
2023-08-27 23:06:08 -0300

    listaExercicios01.sql (4)

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -6,3 +6,7 @@ WHERE nascimento < '1900-01-01';
 SELECT livros.titulo FROM livros
 INNER JOIN autores ON livros.autor_id = autores.id
 AND autores.nome = 'J.K. Rowling';
+
+SELECT alunos.nome FROM alunos
+INNER JOIN matriculas ON alunos.id = matriculas.aluno_id
+AND matriculas.curso = 'Engenharia de Software';
