af19ba35130ef4829600befd9227e5a475f80afd
Muranguinh0 <116584163+Muranguinh0@users.noreply.github.com>
2023-08-27 23:21:08 -0300

    listaExercicios01.sql (12)

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -39,3 +39,7 @@ GROUP BY nome HAVING COUNT(*) = 2;
 SELECT livros.titulo, autores.nome 
 FROM livros LEFT JOIN autores
 ON livros.autor_id = autores.id;
+
+SELECT alunos.nome, matriculas.curso 
+FROM alunos LEFT JOIN matriculas
+ON alunos.id = matriculas.aluno_id;
