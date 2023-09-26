64cd1c7bb03fee18cfe04fb56a256e65566a4f60
Muranguinh0 <116584163+Muranguinh0@users.noreply.github.com>
2023-09-25 23:14:11 -0300

    listaExercicios02.sql (4)
    Correção de outros exercícios

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -7,22 +7,40 @@ END;
 CALL sp_ListarAutores();
 
 DELIMITER //
-CREATE PROCEDURE sp_LivrosPorCategoria(IN Nome_Categoria VARCHAR(100))
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(250))
 BEGIN
 	SELECT Titulo FROM Livro
 	INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
-	AND Categoria.Nome = Nome_Categoria;
+	AND Categoria.Nome = categoria_nome;
 END;
 // DELIMITER ;
 CALL sp_LivrosPorCategoria('Ficção Científica');
 
 DELIMITER //
-CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoria_nome VARCHAR(100))
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoria_nome VARCHAR(250))
 BEGIN
 	SELECT Categoria.Nome, COUNT(*) AS quantidade_livros
-    FROM Livro INNER JOIN Categoria
-    ON Categoria.Categoria_ID = Livro.Categoria_ID 
-    WHERE Categoria.Nome = Nome_Categoria GROUP BY Nome;
+	FROM Livro INNER JOIN Categoria
+	ON Categoria.Categoria_ID = Livro.Categoria_ID 
+	WHERE Categoria.Nome = categoria_nome GROUP BY Nome;
 END;
 // DELIMITER ;
 CALL sp_ContarLivrosPorCategoria('História');
+
+DELIMITER // 
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN categoria_nome VARCHAR(250), OUT possui_livros VARCHAR(50))
+BEGIN
+	DECLARE quantidade_livros INT;
+	SELECT COUNT(*) INTO quantidade_livros
+	FROM Livro INNER JOIN Categoria 
+	ON Categoria.Categoria_ID = Livro.Categoria_ID 
+	WHERE Categoria.Nome = categoria_nome GROUP BY Nome;
+	IF quantidade_livros > 0 THEN
+		SET possui_livros = 'Possui Livros';
+	ELSE
+		SET possui_livros = 'Não Possui Livros';
+	END IF;
+END;
+// DELIMITER ;
+CALL sp_VerificarLivrosCategoria('Romance', @possui_livros);
+SELECT @possui_livros;
