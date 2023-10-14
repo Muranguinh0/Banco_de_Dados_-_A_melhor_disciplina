af9e9654d89fdc96a0ecb126935665b68c71c614
Muranguinh0 <116584163+Muranguinh0@users.noreply.github.com>
2023-10-14 20:12:46 -0300

    Exercício3

--- 4_bimestre/fixacao1.sql
+++ 4_bimestre/fixacao1.sql
@@ -26,3 +26,20 @@ SELECT produto, ROUND(preco, 2) AS preco FROM produtos;
 SELECT produto, ABS(quantidade) AS quantidade FROM produtos;
 
 SELECT produto, AVG(preco) AS media FROM produtos GROUP BY produto;
+
+--Exercício 3--
+CREATE TABLE eventos (
+    data_evento DATE
+);
+INSERT INTO eventos (data_evento) VALUES
+('1997-04-24'),
+('1937-12-25'),
+('1964-06-14'),
+('2006-07-02'),
+('1935-09-29');
+
+INSERT INTO eventos (data_evento) VALUES (NOW());
+
+SELECT MIN(data_evento) AS primeiro_evento, MAX(data_evento) AS segundo_evento, DATEDIFF(MAX(data_evento), MIN(data_evento)) AS diferença_tempo FROM eventos;
+
+SELECT data_evento, DAYNAME(data_evento) AS dia_semana FROM eventos;
