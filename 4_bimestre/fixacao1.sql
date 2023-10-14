621fc2bde6285fe1d9e80c4bfa616a27ad6a915f
Muranguinh0 <116584163+Muranguinh0@users.noreply.github.com>
2023-10-14 20:06:58 -0300

    Exercício2

--- 4_bimestre/fixacao1.sql
+++ 4_bimestre/fixacao1.sql
@@ -13,3 +13,16 @@ SELECT UPPER(nome) AS nomes FROM nomes;
 SELECT nome, LENGTH(nome) AS tamanho FROM nomes;
 
 SELECT CONCAT(IF(nome = 'Roberto' or nome = 'João', 'Sr. ', 'Sra. '), nome) AS nome FROM nomes;
+
+--Exercício 2--
+CREATE TABLE produtos(
+	produto VARCHAR(255),
+	preco DECIMAL(12,5),
+	quantidade INT
+);
+
+SELECT produto, ROUND(preco, 2) AS preco FROM produtos;
+
+SELECT produto, ABS(quantidade) AS quantidade FROM produtos;
+
+SELECT produto, AVG(preco) AS media FROM produtos GROUP BY produto;
