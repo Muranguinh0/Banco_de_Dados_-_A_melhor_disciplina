359733fe718b77274ff15ba930fef2f8f9b8ca78
Muranguinh0 <116584163+Muranguinh0@users.noreply.github.com>
2023-10-25 23:27:42 -0300

    Exerício 3

--- 4_bimestre/exerciciosDeFixacao20231025.sql
+++ 4_bimestre/exerciciosDeFixacao20231025.sql
@@ -7,3 +7,8 @@ CREATE TRIGGER exercicio2
 	AFTER DELETE ON Clientes
 	FOR EACH ROW
 INSERT INTO Auditoria (mensagem) VALUES ('miau');
+
+CREATE TRIGGER exercicio3
+	BEFORE UPDATE ON Clientes
+	FOR EACH ROW
+INSERT INTO Auditoria (mensagem) VALUES (concat(old.nome, new.nome));
