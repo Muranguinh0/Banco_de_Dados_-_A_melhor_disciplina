fc44c299a84441696bf9efa5ce694de6bea0ddc0
Muranguinh0 <116584163+Muranguinh0@users.noreply.github.com>
2023-10-25 23:28:37 -0300

    Exercício 4

--- 4_bimestre/exerciciosDeFixacao20231025.sql
+++ 4_bimestre/exerciciosDeFixacao20231025.sql
@@ -12,3 +12,14 @@ CREATE TRIGGER exercicio3
 	BEFORE UPDATE ON Clientes
 	FOR EACH ROW
 INSERT INTO Auditoria (mensagem) VALUES (concat(old.nome, new.nome));
+
+DELIMITER //
+CREATE TRIGGER exercicio4
+	BEFORE UPDATE ON Clientes
+	FOR EACH ROW
+	IF new.nome is NULL or new.nome = '' THEN
+		UPDATE Clientes SET nome = old.nome;
+		INSERT INTO Auditoria (mensagem) VALUES ('miau');
+	END IF;
+//
+DELIMITER ;
